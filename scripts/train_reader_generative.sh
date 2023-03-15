787f9a91ce981eb074ad4f216e8ad132f9141e01
Atnafu Lambebo Tonja <atnafuatx@gmail.com>
2023-03-15 02:30:51 -0600

    update

--- scripts/train_reader_generative.sh
+++ scripts/train_reader_generative.sh
@@ -1,16 +1,16 @@
-model_name_or_path=ToluClassics/mt5-base
+export HF_TOKEN=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
+model_name_or_path=Atnafu/mt5-base-squad2-fin
 # dataset_name=Tevatron/wikipedia-nq+
 dataset_name=squad_v2
 output_dir=models
-batch_size=64
+batch_size=8
 num_train_epochs=5
 max_seq_length=384
 save_steps=10000
 
-CUDA_VISIBLE_DEVICES=4 python3 baselines/reader/train_seq_2_seq.py \
+CUDA_VISIBLE_DEVICES=0 python3 baselines/reader/train_seq_2_seq.py \
   --model_name_or_path $model_name_or_path \
   --dataset_name $dataset_name \
-  --do_train \
   --do_eval \
   --per_device_train_batch_size $batch_size  \
   --per_device_eval_batch_size $batch_size  \
@@ -22,6 +22,7 @@ CUDA_VISIBLE_DEVICES=4 python3 baselines/reader/train_seq_2_seq.py \
   --save_steps $save_steps \
   --overwrite_output_dir \
   --push_to_hub \
+  --push_to_hub_token $HF_TOKEN \
   --context_column context \
   --question_column question \
   --answer_column answers \
@@ -29,5 +30,7 @@ CUDA_VISIBLE_DEVICES=4 python3 baselines/reader/train_seq_2_seq.py \
   --weight_decay 0.01 \
   --eval_steps 1000 \
   --logging_steps 1000 \
+  --metric_for_best_model="eval_f1,eval_HasAns_f1,eval_NoAns_f1,exact" \
+  --greater_is_better=True \
   --evaluation_strategy="steps" \
   --version_2_with_negative
