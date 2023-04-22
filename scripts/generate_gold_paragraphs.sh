81cec2170f93ca035323cbcd4627377c575b41db
ToluClassics <ogundepoodunayo@gmail.com>
2023-04-21 20:11:47 -0400

    train-test-queries

--- scripts/generate_gold_paragraphs.sh
+++
@@ -1,6 +0,0 @@
-for lang in wol kin yor twi hau fon bem; do
-    mkdir collections/gold_passages/${lang}
-
-    touch collections/gold_passages/${lang}/${lang}_real_links.txt
-    touch collections/gold_passages/${lang}/${lang^}QAAnswers.json
-done
